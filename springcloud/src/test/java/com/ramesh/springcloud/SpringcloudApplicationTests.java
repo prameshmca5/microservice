7cc0beb449594c17d15ff37899c79c0db1f678ed
Ramesh Pongiannan <rameshkp2@gmail.com>
2024-06-01 00:05:35 +0800

    Added new code into discovery services.

---
+++ springcloud/src/test/java/com/ramesh/springcloud/SpringcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramesh.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
